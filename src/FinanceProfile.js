79d97206ade19b7b9a983ef6984fe6ba7bb047d0
austin imbastari <austin imbastari>
2017-05-08 19:49:28 -0700

    create various screens

---
+++ src/FinanceProfile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Test } from 'react-native';
+
+class FinanceProfile extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Finance Profile </Text>
+            </View>
+        );
+    }
+}
+
+export default FinanceProfile;
---
+++ src/LoginForm.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class LoginForm extends Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    Login Form
+                </Text>        
+
+            </View>
+
+        );
+    }
+}
+
+export default LoginForm;
